a9c782a9356dc6b68dd8280fd831cbbd11d6cc57
Clay <clay.degruchy+github@gmail.com>
2025-06-09 16:05:36 +0200

    got hosting mnostly working

--- scripts/global.gd
+++ scripts/global.gd
@@ -10,7 +10,9 @@ var lobby_members: Array = []
 
 var lobby_id: int
 
-var player_name: String
+
+signal connection_state(state)
+signal lobby_list_update(lobbies)
 
 
 func _init():
@@ -31,6 +33,7 @@ func _ready():
 	print("setting up callbacks...")
 	Steam.lobby_created.connect(_on_lobby_created)
 	Steam.lobby_joined.connect(_on_lobby_joined)
+	Steam.lobby_match_list.connect(_lobby_match_list)
 
 
 	multiplayer.connected_to_server.connect(on_multiplayer_connect)
@@ -45,40 +48,63 @@ func _process(_delta):
 
 
 func _create_lobby():
+	print("_create_lobby")
 	Steam.createLobby(Steam.LOBBY_TYPE_PUBLIC, lobby_members_max)
 
-func _join_lobby(lobby_id: int, new_player_name: String):
-	player_name = new_player_name
+func _join_lobby(lobby_id: int, ):
+	print("_join_lobby")
 	Steam.joinLobby(int(lobby_id))
 	
 func _on_lobby_created(_connect: int, this_lobby_id: int):
+	print("_on_lobby_created")
 	if _connect:
 		lobby_id = this_lobby_id
 		Steam.setLobbyData(lobby_id, "name", steam_username + "'s lobby")
 		Steam.setLobbyJoinable(lobby_id, true)
-		create_socket()
+		create_multiplayer_socket()
 		print("Create lobby id:", str(lobby_id))
 
+
 func _on_lobby_joined(lobby: int, permissions: int, locked: bool, response: int):
+	print("_on_lobby_joined")
 	if response == 1:
 		var id = Steam.getLobbyOwner(lobby)
 		if id != Steam.getSteamID():
-			connect_socket(id)
+			connect_multiplayer_socket(id)
+		lobby_id = id
 
 
-func create_socket():
+func create_multiplayer_socket():
+	print("create_multiplayer_socket")
 	# Example of peer config
 	#peer.set_config(SteamPeerConfig.NETWORKING_CONFIG_SEND_BUFFER_SIZE, 524288)
 	var error = peer.create_host(0)
 	if error != OK:
 		print("ERROR")
 		print(error)
-	print(peer)
 	multiplayer.multiplayer_peer = peer
 
 
-func connect_socket(steam_id: int):
+func connect_multiplayer_socket(steam_id: int):
+	print("connect_multiplayer_socket")
 	# Example of peer config
 	# peer.set_config(SteamPeerConfig.NETWORKING_CONFIG_SEND_BUFFER_SIZE, 524288)
 	peer.create_client(steam_id, 0)
 	multiplayer.multiplayer_peer = peer
+
+
+func get_lobby_list():
+	print("get_lobby_list")
+	Steam.addRequestLobbyListDistanceFilter(Steam.LOBBY_DISTANCE_FILTER_CLOSE)
+	Steam.requestLobbyList()
+
+func _lobby_match_list(lobbies):
+	print("_lobby_match_list")
+	var t = []
+	for lobby in lobbies:
+		var lobby_name = Steam.getLobbyData(lobby, "name")
+		if lobby_name.length() < 1:
+			continue
+		var member_count = Steam.getNumLobbyMembers(lobby)
+		t.append([lobby_name, member_count])
+	lobby_list_update.emit(t)
--- scripts/scene.gd
+++ scripts/scene.gd
@@ -1,38 +1,46 @@
 extends Node3D
 
-# var multiplayer_peer = ENetMultiplayerPeer.new()
-# var peer = SteamMultiplayerPeer.new()
-
 
 @onready var menu = $Menu
 
 
+@onready var sessions_browser: VBoxContainer = %SessionsContainer
+
+
 func _ready():
+	Global.lobby_list_update.connect(update_session_browser)
+	Global.peer.peer_connected.connect(func(id): add_player_character(id))
 	pass
+
 
 func _on_host_pressed():
 	print("_on_host_pressed")
 	Global._create_lobby()
-
-	# multiplayerr
-	# multiplayer_peer.create_server(port)
-	# multiplayer.multiplayer_peer = multiplayer_peer
-	# multiplayer_peer.peer_connected.connect(func(id): add_player_character(id))
-	# menu.visible = false
-	# add_player_character()
 	
 
-func _on_join_pressed():
-	print("_on_join_pressed")
-	# Global._join_lobby()
-	# peer.create_client(
-	# multiplayer_peer.create_client("localhost", port)
-	# multiplayer.multiplayer_peer = multiplayer_peer
-	# menu.visible = false
+func get_sessions():
+	print("get_sessions")
+	Global.get_lobby_list()
 
 
 func add_player_character(id = 1):
 	print("add_player_character")
-	# var character = preload("res://player/player.tscn").instantiate()
-	# character.name = str(id)
-	# add_child(character)
+	var character = preload("res://player/player.tscn").instantiate()
+	character.name = str(id)
+	add_child(character)
+
+
+func update_session_browser(lobbies: Array):
+	print("update_session_browser")
+	print(lobbies)
+
+	var children = sessions_browser.get_children()
+	for child in children:
+		child.free()
+
+	for lobby in lobbies:
+		var b = Button.new()
+		b.text = lobby[0]
+		b.pressed.connect(Global._join_lobby.bind(lobby[1]))
+		sessions_browser.add_child(b)
+	return
